0713b648120ff0e71ddbddbf927672e0640a59f2
zarifahdara <zarifahdaraningtias@gmail.com>
2026-06-03 18:50:01 +0700

    Menambahkan fitur CRUD produk

--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -5,10 +5,78 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\ProductModel;
 class ProdukController extends BaseController
 {
+    protected $productModel; 
+
+function __construct()
+{
+    helper('form');
+    $this->productModel = new ProductModel();
+}
     public function index()
     {
-        return view('v_produk');
+        return view('produk/index', [
+    'products' => $this->productModel->findAll()
+]);
     }
+    public function create()
+{
+    $dataFoto = $this->request->getFile('foto');
+
+    $dataForm = [
+        'nama' => $this->request->getPost('nama'),
+        'harga' => $this->request->getPost('harga'),
+        'jumlah' => $this->request->getPost('jumlah') 
+    ];
+
+    if ($dataFoto->isValid()) {
+        $fileName = $dataFoto->getRandomName(); 
+        $dataFoto->move('img/', $fileName);
+        
+        $dataForm['foto'] = $fileName;
+    }
+
+    $this->productModel->insert($dataForm);
+
+    return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+} 
+public function edit($id)
+{
+    $dataProduk = $this->productModel->find($id);
+
+    $dataForm = [
+        'nama' => $this->request->getPost('nama'),
+        'harga' => $this->request->getPost('harga'),
+        'jumlah' => $this->request->getPost('jumlah') 
+    ];
+
+    if ($this->request->getPost('check') == 1) {
+        if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+            unlink("img/" . $dataProduk['foto']);
+        }
+
+        $dataFoto = $this->request->getFile('foto');
+
+        if ($dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName();
+            $dataFoto->move('img/', $fileName);
+            
+            $dataForm['foto'] = $fileName;
+        }
+    }
+
+    $this->productModel->update($id, $dataForm);
+
+    return redirect('produk')->with('success', 'Data Berhasil Diubah');
+}
+
+public function delete($id)
+{
+    $dataProduk = $this->productModel->find($id);
+    $this->productModel->delete($id);
+
+    return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+}
 }
---
+++ app/Views/produk/index.php
@@ -0,0 +1,65 @@
+<?= $this->extend('layout') ?>
+<?= $this->section('content') ?> 
+<?php
+if (session()->getFlashData('success')) {
+?>
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <?= session()->getFlashData('success') ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php
+}
+?>
+<?php
+if (session()->getFlashData('failed')) {
+?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <?= session()->getFlashData('failed') ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php
+}
+?>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+    Tambah Data
+</button>
+<!-- Table with stripped rows -->
+<table class="table datatable">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nama</th>
+            <th scope="col">Harga</th>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Foto</th>
+            <th scope="col"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($products as $index => $produk) : ?>
+            <tr>
+                <th scope="row"><?php echo $index + 1 ?></th>
+                <td><?php echo $produk['nama'] ?></td>
+                <td><?php echo $produk['harga'] ?></td>
+                <td><?php echo $produk['jumlah'] ?></td>
+                <td>
+                    <?php if ($produk['foto'] != '' and file_exists("img/" . $produk['foto'] . "")) : ?>
+                        <img src="<?php echo base_url() . "img/" . $produk['foto'] ?>" width="100">
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editModal-<?= $produk['id'] ?>">
+    Ubah
+</button>
+<a href="<?= base_url('produk/delete/' . $produk['id']) ?>" class="btn btn-danger" onclick="return confirm('Yakin hapus data ini ?')">
+    Hapus
+</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
+<!-- End Table with stripped rows --> 
+ <?= $this->include('produk/modal_add') ?>
+ <?= $this->include('produk/modal_edit') ?>
+<?= $this->endSection() ?>
